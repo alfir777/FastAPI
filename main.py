b76be1fbf305949ed638095d65cc8dc061537722
Alfir Tyakin <alfir777@rambler.ru>
2021-09-23 13:24:22 +0500

    request body and pydantic model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schemas import Book
 
 
 app = FastAPI()
@@ -7,3 +8,18 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {'key': 'Hello'}
+
+
+@app.get('/{pk}')
+def get_item(pk: int, q: str = None):
+    return {'key': pk, 'q': q}
+
+
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {'user': pk, 'item': item}
+
+
+@app.post('/book')
+def create_book(item: Book):
+    return item
