7d97c7a1fc3996c88548cb673fb71cecb7c48aaf
Alfir Tyakin <alfir777@rambler.ru>
2021-09-23 14:43:07 +0500

    add Body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Query, Path
-from schemas import Book
-
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author
 
 app = FastAPI()
 
@@ -21,8 +20,13 @@ def get_user_item(pk: int, item: str):
 
 
 @app.post('/book')
-def create_book(item: Book):
-    return item
+def create_book(item: Book, author: Author, quantity: int = Body(...)):
+    return {'item': item, 'author': author, 'quantity': quantity}
+
+
+@app.post('/author')
+def create_author(author: Author = Body(..., embed=True)):
+    return {'author': author}
 
 
 @app.get('/book')
